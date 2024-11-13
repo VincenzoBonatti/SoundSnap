976176ed407268f9539ad35c28b4b4476ff4989b
Vincenzo Bonatti <vincenzobonati@outlook.com>
2024-11-12 21:57:01 -0300

    Cadastro funcionando

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,16 @@
-function cadastro(params) {
-    
-}
+const cadastro = document.getElementById("cadastro")
+
+cadastro.addEventListener('submit', evento =>{
+    evento.preventDefault()
+
+    const formData = new FormData(cadastro)
+    const data = Object.fromEntries(formData)
+
+    fetch("http://127.0.0.1:8000/users/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "Application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(alert("USUÁRIO CRIADO COM SUCESSO")).then(window.location.replace('/'))
+})
