70b79210e0d793409c7592954de605ec32a35d7c
Vincenzo Bonatti <133982612+VincenzoBonatti@users.noreply.github.com>
2024-11-14 00:12:24 +0000

    get do login funcionando

--- cadastro.js
+++ cadastro.js
@@ -6,11 +6,35 @@ cadastro.addEventListener('submit', evento =>{
     const formData = new FormData(cadastro)
     const data = Object.fromEntries(formData)
 
-    fetch("http://127.0.0.1:8000/users/", {
+    fetch("https://chilling-spooky-superstition-9p4qq97g9wg395p5-8000.app.github.dev/users/", {
         method: "POST",
         headers: {
             "Content-Type": "Application/json"
         },
         body: JSON.stringify(data)
     }).then(alert("USUÁRIO CRIADO COM SUCESSO")).then(window.location.replace('/'))
+})
+
+
+const login = document.getElementById("login")
+
+login.addEventListener('submit', evento =>{
+    evento.preventDefault()
+
+    const formData = new FormData(login)
+    const data = Object.fromEntries(formData)
+
+    fetch(`https://chilling-spooky-superstition-9p4qq97g9wg395p5-8000.app.github.dev/users/${data.usuario}`, {
+        headers: {
+            "Content-Type": "Application/json",
+            
+        }}).then(data => data.json()).then(response => {
+            if (data.senha == response.senha) {
+
+                //
+                alert("logado com sucesso").then(window.location.replace('/'))
+            } else if(data.senha != response.senha){
+                alert("Senha incorreta")
+            }
+        })
 })
