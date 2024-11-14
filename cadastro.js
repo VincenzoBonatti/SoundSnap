2e36e5ea2092a05fc65403a07a4f8d07d6df19b5
Vincenzo Bonatti <133982612+VincenzoBonatti@users.noreply.github.com>
2024-11-14 00:55:17 +0000

    melhorias cadastro(precisa testar) get do login funcionando(falta sistema de login)

--- cadastro.js
+++ cadastro.js
@@ -4,15 +4,27 @@ cadastro.addEventListener('submit', evento =>{
     evento.preventDefault()
 
     const formData = new FormData(cadastro)
-    const data = Object.fromEntries(formData)
+    const form = Object.fromEntries(formData)
 
-    fetch("https://chilling-spooky-superstition-9p4qq97g9wg395p5-8000.app.github.dev/users/", {
-        method: "POST",
+     fetch(`https://chilling-spooky-superstition-9p4qq97g9wg395p5-8000.app.github.dev/users/${form.usuario}`, {
         headers: {
-            "Content-Type": "Application/json"
-        },
-        body: JSON.stringify(data)
-    }).then(alert("USUÁRIO CRIADO COM SUCESSO")).then(window.location.replace('/'))
+            "Content-Type": "Application/json",
+            "Access-Control-Allow-Origin": "*"
+        }}).then(data => {
+            if (data.status == 404) {            
+                    fetch("https://chilling-spooky-superstition-9p4qq97g9wg395p5-8000.app.github.dev/users/", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "Application/json"
+                        },
+                        body: JSON.stringify(form)
+                    }).then(data => data.json()).then(response => console.log(response)).then(alert("USUÁRIO CRIADO COM SUCESSO"))/*.then(window.location.replace('/'))*/
+            } else if (data.status == 200){
+                alert("Este nome de usuário já está em uso")
+            }
+        })
+
+    
 })
 
 
@@ -27,14 +39,19 @@ login.addEventListener('submit', evento =>{
     fetch(`https://chilling-spooky-superstition-9p4qq97g9wg395p5-8000.app.github.dev/users/${data.usuario}`, {
         headers: {
             "Content-Type": "Application/json",
-            
+            "Access-Control-Allow-Origin": "*"
         }}).then(data => data.json()).then(response => {
-            if (data.senha == response.senha) {
+            console.log(response)
+            if (response.detail == "User not found"){
+                alert("Este usuário não existe")
+            }else if (data.senha == response.senha) {
 
                 //
-                alert("logado com sucesso").then(window.location.replace('/'))
+                alert("logado com sucesso")
+                /*.then(window.location.replace('/'))*/
             } else if(data.senha != response.senha){
                 alert("Senha incorreta")
             }
+            
         })
 })
